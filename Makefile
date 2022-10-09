16345a600745cdb8530780ff68db0ec7f468e315
hardfist <yangjianzju@gmail.com>
2022-10-09 15:21:08 +0800

    chore: use hyperfine

--- Makefile
+++ Makefile
@@ -8,4 +8,11 @@ spack:
 	@hyperfine --warmup 3 './node_modules/@swc/cli/bin/spack node_modules/three/src/Three.js'
 swc:
 	@cargo build --release
-	@./target/release/swc 
+	@RAYON_NUM_THREADS=1 hyperfine --warmup 3 './target/release/swc_single'
+	@RAYON_NUM_THREADS=1 hyperfine --warmup 3 './target/release/swc_parallel'
+	@RAYON_NUM_THREADS=2 hyperfine --warmup 3 './target/release/swc_single'
+	@RAYON_NUM_THREADS=2 hyperfine --warmup 3 './target/release/swc_parallel'
+	@RAYON_NUM_THREADS=4 hyperfine --warmup 3 './target/release/swc_single'
+	@RAYON_NUM_THREADS=4 hyperfine --warmup 3 './target/release/swc_parallel'
+	@RAYON_NUM_THREADS=8 hyperfine --warmup 3 './target/release/swc_single'
+	@RAYON_NUM_THREADS=8 hyperfine --warmup 3 './target/release/swc_parallel'
--- src/bin/swc_parallel.rs
+++ src/bin/swc_parallel.rs
@@ -5,11 +5,9 @@ use rayon::prelude::*;
 use std::env;
 use std::fs;
 use std::sync::Arc;
-use std::time::Instant;
 use swc_common::{self, sync::Lrc};
 use swc_ecma_parser::{parse_file_as_module};
 fn main() {
-    let whole_start = Instant::now();
     let cwd = env::current_dir().unwrap();
     let globs = glob_with(
         "node_modules/three/src/**/*",
@@ -30,25 +28,11 @@ fn main() {
         }
         
     }).collect();
-    let codes:Vec<_> = codes.iter().cycle().take(10*codes.len()).collect();
+    // let codes:Vec<_> = codes.iter().cycle().take(10*codes.len()).collect();
     let cm: Lrc<SourceMap> = Arc::new(Default::default());
-    let start = Instant::now();
-    
-    for (path,_code) in codes.iter() {
-        let fm = cm.load_file(&path).expect("load file failed: {}");
-        let _program = parse_file_as_module(&fm, Default::default(), Default::default(), Default::default(), &mut vec![]).unwrap();
-    }
-    let duration = start.elapsed();
-    println!("serialize duration: {:?}", duration);
-    let cm: Lrc<SourceMap> = Default::default();
-    let start = Instant::now();
     let _result:Vec<_> = codes.par_iter().map(|(path,_code)| {
         let fm = cm.load_file(&path).expect("load file failed: {}");
         let program = parse_file_as_module(&fm, Default::default(), Default::default(), Default::default(), &mut vec![]).unwrap();
         program
     }).collect();
-    let duration = start.elapsed();
-    println!("parallel duration: {:?}", duration);
-    let duration = whole_start.elapsed();
-    println!("whole duration: {:?}", duration);
 }
