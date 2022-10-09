f53cfbc21067d7829bb7a1e0537db25bbe0598bf
hardfist <yangjianzju@gmail.com>
2022-10-09 15:28:49 +0800

    chore: fix

--- build.js
+++ build.js
@@ -5,42 +5,43 @@ const esbuild = require('esbuild');
 const fs = require('fs');
 const babel = require('@babel/core');
 const files = glob.sync('./node_modules/three/src/**/*', { absolute: true, nodir: true });
-const list = files.map((x) => fs.readFileSync(x, 'utf-8'));
+const list = files.map((x) => ({ file: x, code: fs.readFileSync(x, 'utf-8') }));
+console.log(list[0].file);
 let codes = [];
 for (let i = 0; i < 10; i++) {
   codes = codes.concat(list);
 }
 async function main() {
   console.time('swc_code');
-  for (const code of codes) {
+  for (const { _, code } of codes) {
     swc.parseSync(code);
   }
   console.timeEnd('swc_code');
   console.time('swc_serial');
-  for (const file of files) {
+  for (const { file } of codes) {
     await swc.parseFile(file);
   }
   console.timeEnd('swc_serial');
   console.time('swc_parallel');
   let queue = [];
-  for (const file of files) {
+  for (const { file } of codes) {
     queue.push(swc.parseFile(file));
   }
   await Promise.all(queue);
   console.timeEnd('swc_parallel');
   console.time('acorn');
-  for (const code of codes) {
+  for (const { code } of codes) {
     acorn.parse(code, { sourceType: 'module', ecmaVersion: 'latest' });
   }
   console.timeEnd('acorn');
   console.time('babel');
-  for (const code of codes) {
+  for (const { code } of codes) {
     babel.parse(code);
   }
   console.timeEnd('babel');
 
   console.time('esbuild');
-  for (const code of codes) {
+  for (const { code } of codes) {
     await esbuild.transform(code);
   }
   console.timeEnd('esbuild');
